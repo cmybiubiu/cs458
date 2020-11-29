7c4ad50584aa891317aceba40ed3a0e9ced1e3c3
cmybiubiu <cmyhamster@gmail.com>
2020-11-28 21:26:00 -0800

    hiii

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ set -e
 
 time=60
 bwnet=10
+bwhost=1000
 # TODO: If you want the RTT to be 4ms what should the delay on each
 # link be?  Set this value correctly.
 delay=1
@@ -16,7 +17,7 @@ modprobe tcp_probe
 # make sure we don't use a cached cwnd
 sysctl -w net.ipv4.tcp_no_metrics_save=1
 
-for qsize in 20 100; do
+for qsize in 5 20 100; do
     dir=bb-q$qsize
 
     python bufferbloat.py --dir=$dir --time=$time --bw-net=$bwnet --delay=$delay --maxq=$qsize
